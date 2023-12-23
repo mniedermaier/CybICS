52c44881c40b645b78917a7684337e99b3da52ee
Matthias Niedermaier <matthias.niedermaier@googlemail.com>
2023-12-23 21:18:37 +0100

    Update on devcontainer and historian

--- software/historian/historian.py
+++ software/historian/historian.py
@@ -3,11 +3,13 @@ import time
 import os
 from pymodbus.client import ModbusTcpClient
 
-path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+#path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+path = "./"
 db_path = os.path.join(path, "historian.sqlite")
 conn = None
 
 def get_db():
+    print("DB path: " + str(db_path))
     conn = sqlite3.connect(db_path)
     return conn
 
@@ -23,27 +25,34 @@ def reset_db():
 
     db.commit()
 
-db = get_db()
-reset_db()
 
-# Main loop to read each sensor and save the readings in the database.
-print('Saving sensor data every two seconds (press Ctrl-C to quit)...')
-while True:
-    # Save the current unix time for this measurement.
-    reading_time = int(time.time())
+
+if __name__ == '__main__':
+    # Main loop to read each sensor and save the readings in the database.
+    print('Saving historian data every second (press Ctrl-C to quit)...')
+
+    db = get_db()
+    reset_db()
+
     client = ModbusTcpClient(host="192.168.178.141",port=502)   # Create client object
     client.connect()                           # connect to device, reconnect automatically
-    request = client.read_holding_registers(1124,1)
-    gst = request.registers
-    print("GST: " + str(gst[0]))
-    request = client.read_holding_registers(1125,1)
-    hpt = request.registers
-    print("HPT: " + str(hpt[0]))
 
-    # Save the reading in the readings table.
-    db.execute('INSERT INTO readings VALUES (?, ?, ?)',
-             (reading_time, "GST", int(gst[0])))
-    db.execute('INSERT INTO readings VALUES (?, ?, ?)',
-             (reading_time, "HPT", int(hpt[0])))
-    db.commit()
-    time.sleep(2.0)
+    while True:
+        # Save the current unix time for this measurement.
+        reading_time = int(time.time())
+
+        request = client.read_holding_registers(1124,1)
+        gst = request.registers
+        print("GST: " + str(gst[0]))
+        request = client.read_holding_registers(1125,1)
+        hpt = request.registers
+        print("HPT: " + str(hpt[0]))
+
+        # Save the reading in the readings table.
+        db.execute('INSERT INTO readings VALUES (?, ?, ?)',
+                (reading_time, "GST", int(gst[0])))
+        db.commit()
+        db.execute('INSERT INTO readings VALUES (?, ?, ?)',
+                (reading_time, "HPT", int(hpt[0])))
+        db.commit()
+        time.sleep(1.0)
