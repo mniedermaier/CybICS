a7a8480569e524198641faabac37240fd80f13d1
Matthias Niedermaier <matthias.niedermaier@googlemail.com>
2024-01-29 23:33:12 +0100

    Raspberry Pi install script can now flash the STM32

--- software/installRPI.sh
+++ software/installRPI.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
 
+# define colors
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+MAGENTA="\033[35m"
+ENDCOLOR="\e[0m"
+
 GIT_ROOT=$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")
 
 echo "$GIT_ROOT"
 source "$GIT_ROOT"/.dev.env
 
+echo -ne "${MAGENTA}"
 echo "                                    "
 echo " Staring the                        "
 echo "                                    "
@@ -21,11 +29,15 @@ echo "                                    "
 echo " Important:                         "
 echo " Make sure, that the ENV variables  "
 echo " are set correctly! (../.dev.env)   "
+echo -ne "${ENDCOLOR}"
 
+# if false; then
+# fi
 
 ###
-### Remove IP from known_hosts
+### Remove IP from known_hosts and copy ssh key
 ###
+echo -ne "${YELLOW}# Type in Raspberry Pi password, when requested (this will copy your SSH key to it): \n${ENDCOLOR}" 
 ssh-keygen -f ~/.ssh/known_hosts -R "$DEVICE_IP"
 ssh-keyscan -H "$DEVICE_IP" >> ~/.ssh/known_hosts
 ssh-copy-id -i ~/.ssh/id_rsa.pub "$DEVICE_USER"@"$DEVICE_IP"
@@ -34,20 +46,20 @@ ssh-copy-id -i ~/.ssh/id_rsa.pub "$DEVICE_USER"@"$DEVICE_IP"
 ###
 ### Copying CybICS Git to the target
 ###
-echo "# Removing old CybICS GIT, if existing:" 
+echo -ne "${GREEN}# Removing old CybICS GIT, if existing: \n${ENDCOLOR}" 
 ssh "$DEVICE_USER"@"$DEVICE_IP" /bin/bash << EOF
     rm -rf /home/pi/gits/CybICS
     mkdir -p /home/pi/gits
 EOF
 
-echo "# Copying CybICS GIT to Raspberry Pi"
+echo -ne "${GREEN}# Copying CybICS GIT to Raspberry Pi \n${ENDCOLOR}"
 scp -rp "$GIT_ROOT" "$DEVICE_USER"@"$DEVICE_IP":/home/pi/gits
 
 
 ###
 ### FUXA installation
 ###
-echo "# Increasing swap file ..."
+echo -ne "${GREEN}# Increasing swap file ... \n${ENDCOLOR}"
 ssh "$DEVICE_USER"@"$DEVICE_IP" /bin/bash << EOF
     set -e
     sudo dphys-swapfile swapoff
@@ -56,7 +68,7 @@ ssh "$DEVICE_USER"@"$DEVICE_IP" /bin/bash << EOF
     sudo dphys-swapfile swapon
 EOF
 
-echo "# Installing FUXA ..."
+echo -ne "${GREEN}# Installing FUXA ... \n${ENDCOLOR}"
 ssh "$DEVICE_USER"@"$DEVICE_IP" /bin/bash << EOF
     set -e
     sudo apt-get update
@@ -64,7 +76,7 @@ ssh "$DEVICE_USER"@"$DEVICE_IP" /bin/bash << EOF
     sudo npm install -g --unsafe-perm @frangoteam/fuxa
 EOF
 
-echo "# Config FUXA ..."
+echo -ne "${GREEN}# Config FUXA ... \n${ENDCOLOR}"
 ssh "$DEVICE_USER"@"$DEVICE_IP" /bin/bash << EOF
     set -e
     sudo systemctl stop fuxa.service | true    
@@ -89,7 +101,7 @@ EOL
     sudo systemctl enable fuxa.service
 EOF
 
-echo "# Config FUXA project ..."
+echo -ne "${GREEN}# Config FUXA project ... \n${ENDCOLOR}"
 while ! curl -X POST -H "Content-Type: application/json" -d @"$GIT_ROOT"/software/FUXA/fuxa-project.json http://"$DEVICE_IP":1881/api/project
 do
     echo "Waiting till FUXA is online ..."
@@ -100,7 +112,7 @@ done
 ###
 ### OpenPLC installation
 ###
-echo "# Installing OpenPLC ..."
+echo -ne "${GREEN}# Installing OpenPLC ... \n${ENDCOLOR}"
 ssh "$DEVICE_USER"@"$DEVICE_IP" /bin/bash << EOF
     set -e
     sudo apt-get install pigpio git -y
@@ -125,13 +137,13 @@ EOF
 ###
 ### Enable I2C
 ###
-echo "# Enable I2C on the RPi ..."
+echo -ne "${GREEN}# Enable I2C on the RPi ... \n${ENDCOLOR}"
 ssh "$DEVICE_USER"@"$DEVICE_IP" /bin/bash << EOF
     set -e
     sudo raspi-config nonint do_i2c 0
 EOF
 
-echo "# Config I2C script ..."
+echo -ne "${GREEN}# Config I2C script ... \n${ENDCOLOR}"
 ssh "$DEVICE_USER"@"$DEVICE_IP" /bin/bash << EOF
     set -e
     sudo apt-get install python3-netifaces python3-pymodbus python3-smbus -y
@@ -161,18 +173,34 @@ EOF
 ###
 ### Installing GCC ARM
 ###
-echo "# Installing GCC ARM NONE EABI on the RPi ..."
+echo -ne "${GREEN}# Installing GCC ARM NONE EABI on the RPi ... \n${ENDCOLOR}"
 ssh "$DEVICE_USER"@"$DEVICE_IP" /bin/bash << EOF
     set -e
     sudo apt-get install gcc-arm-none-eabi -y
 EOF
 
-
 ###
 ### Installing openocd
 ###
-echo "# Installing openocd on the RPi ..."
+echo -ne "${GREEN}# Installing openocd on the RPi ... \n${ENDCOLOR}"
 ssh "$DEVICE_USER"@"$DEVICE_IP" /bin/bash << EOF
     set -e
     sudo apt-get install openocd -y
 EOF
+
+###
+### Building and flashing STM32 software
+###
+echo -ne "${GREEN}# Building and flashing STM32 software ... \n${ENDCOLOR}"
+ssh "$DEVICE_USER"@"$DEVICE_IP" /bin/bash << EOF
+    cd /home/pi/gits/CybICS/software/stm32
+    make clean
+    make -j4
+    sudo openocd -f ./openocd_rpi.cfg -c "program build/CybICS.bin verify reset exit 0x08000000"
+EOF
+
+###
+### all done
+###
+echo -ne "${GREEN}# All done, ready to rubmle ... \n${ENDCOLOR}"
+
