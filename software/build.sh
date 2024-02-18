1cc26cfaa0a7a92f5a17c6b664b38d8a84c6a6bf
Manuel Liebert <man.liebert@gmail.com>
2024-02-18 18:09:32 +0000

    Build stm32 in container and flash on rpi

--- software/build.sh
+++ software/build.sh
@@ -15,4 +15,4 @@ docker buildx inspect --bootstrap
 docker buildx build --platform linux/arm64 -t localhost:5000/cybics-readi2c:latest --push ./scripts
 docker buildx build --platform linux/arm64 -t localhost:5000/cybics-openplc:latest --push ./OpenPLC
 docker buildx build --platform linux/arm64 -t localhost:5000/cybics-fuxa:latest --push ./FUXA
-docker buildx build --platform linux/arm64 -t localhost:5000/cybics-openocd:latest --push ./openocd
+docker buildx build --platform linux/arm64 -t localhost:5000/cybics-stm32:latest --push ./stm32
