94f0df1f5389adf2a26a303e551e201fe88678a9
Manuel Liebert <man.liebert@gmail.com>
2024-08-14 14:55:47 +0000

    Align devcontainer and container names

--- software/build.sh
+++ software/build.sh
@@ -8,7 +8,7 @@ docker buildx ls | grep -q $name && docker buildx use $name
 docker buildx ls | grep -q $name || docker buildx create  --use --config config.toml --name $name
 docker buildx inspect --bootstrap
 
-docker buildx build --platform linux/arm64 -t 172.17.0.1:5000/cybics-hwio-rpi:latest --push ./hwio-rpi
+docker buildx build --platform linux/arm64 -t 172.17.0.1:5000/cybics-hwio-raspberry:latest --push ./hwio-raspberry
 docker buildx build --platform linux/arm64 -t 172.17.0.1:5000/cybics-openplc:latest --push ./OpenPLC
 docker buildx build --platform linux/arm64 -t 172.17.0.1:5000/cybics-opcua:latest --push ./opcua
 docker buildx build --platform linux/arm64 -t 172.17.0.1:5000/cybics-fuxa:latest --push ./FUXA
