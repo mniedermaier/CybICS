d0e05d6bb3b65d445d6e9db6544b41cb9b372a26
Manuel Liebert <man.liebert@gmail.com>
2024-08-14 09:02:29 +0000

    Dont use network host on build rpi containers - this prevents conflicts when running local services with open ports

--- software/build.sh
+++ software/build.sh
@@ -3,12 +3,13 @@
 set -e
 cd "$(dirname "$0")"
 
-name=cybicsbuilder
-docker buildx ls | grep -q $name || docker buildx create --driver-opt network=host --use --config config.toml --name $name
+name=cybicsbuilder2
+docker buildx ls | grep -q $name && docker buildx use $name
+docker buildx ls | grep -q $name || docker buildx create  --use --config config.toml --name $name
 docker buildx inspect --bootstrap
 
-docker buildx build --platform linux/arm64 -t localhost:5000/cybics-hwio-rpi:latest --push ./hwio-rpi
-docker buildx build --platform linux/arm64 -t localhost:5000/cybics-openplc:latest --push ./OpenPLC
-docker buildx build --platform linux/arm64 -t localhost:5000/cybics-opcua:latest --push ./opcua
-docker buildx build --platform linux/arm64 -t localhost:5000/cybics-fuxa:latest --push ./FUXA
-docker buildx build --platform linux/arm64 -t localhost:5000/cybics-stm32:latest --push ./stm32
+docker buildx build --platform linux/arm64 -t 172.17.0.1:5000/cybics-hwio-rpi:latest --push ./hwio-rpi
+docker buildx build --platform linux/arm64 -t 172.17.0.1:5000/cybics-openplc:latest --push ./OpenPLC
+docker buildx build --platform linux/arm64 -t 172.17.0.1:5000/cybics-opcua:latest --push ./opcua
+docker buildx build --platform linux/arm64 -t 172.17.0.1:5000/cybics-fuxa:latest --push ./FUXA
+docker buildx build --platform linux/arm64 -t 172.17.0.1:5000/cybics-stm32:latest --push ./stm32
