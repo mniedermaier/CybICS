53eb16de9c452b287a8e1bf7c624bffc0fc938fa
Matthias Niedermaier <matthias.niedermaier@googlemail.com>
2025-10-24 09:37:24 +0200

    Version push

--- software/stm32/Core/Src/main.c
+++ software/stm32/Core/Src/main.c
@@ -710,7 +710,7 @@ void Fdisplay(void const * argument)
     secondsAfterStart++;
     // Display showing Cybics string and IP
     if(0==displayScreen){
-      snprintf(displayText, sizeof(displayText), "%-16s", "CybICS v1.1.1");
+      snprintf(displayText, sizeof(displayText), "%-16s", "CybICS v1.1.2");
       Lcd_cursor(&lcd, 0, 0);
       Lcd_string(&lcd, displayText);
       snprintf(displayText, sizeof(displayText), "%16li", secondsAfterStart);
